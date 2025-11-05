cd90bc03e24a473cb8e5f2d50db26e21bf7b0a32
Agustin Tabarcache <agustintabarcache74@gmail.com>
2025-11-05 18:48:41 -0300

    Agregamos React-Router-DOM para el manejo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 const App = () => {
@@ -7,10 +8,14 @@ const App = () => {
 
   return (
     <div className="container mx-auto mt-5 max-w-1/2">
-      <h1 className="text-4xl font-bold text-center">Tareas</h1>
+      <h1 className="text-4xl font-bold text-center">Manager de Tareas</h1>
       
-      <TaskForm />
-      <TaskList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TaskList />}/>
+          <Route path="/create-task" element={<TaskForm />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
