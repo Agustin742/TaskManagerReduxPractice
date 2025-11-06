60a0e43759864fc39660c21d3469fc88e86d9b15
Agustin Tabarcache <agustintabarcache74@gmail.com>
2025-11-06 00:03:00 -0300

    Agregamos navigate para poder movernos en rutas

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { deleteTask } from "../store/slices/task"
+import { Link } from "react-router-dom"
 
 
 const TaskList = () => {
@@ -8,12 +9,22 @@ const TaskList = () => {
     // console.log(stateTasks);
     const dispatch = useDispatch()
 
+
+
     const handleDelete = (id) => {
     dispatch(deleteTask(id))
     }
 
     return (
     <div className="mt-5">
+
+      <header>
+        <h2>Toltal de Tareas: {stateTasks.length}</h2>
+
+        <Link to={'/create-task'}>
+        Crear Tarea
+        </Link>
+      </header>
         {stateTasks && stateTasks.map((task) => (
             <div
             className=" border border-gray-700 m-2 rounded-sm p-2 transition-all hover:scale-105 hover:shadow-md duration-500 focus:border-transparent"
